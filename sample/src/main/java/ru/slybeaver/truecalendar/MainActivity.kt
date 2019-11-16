53a53edaa51c1b18fd37f49a52d495897817f57a
andreu21-dev <andreu21@protonmail.com>
2019-11-16 13:54:35 +0300

    little changes in sample

---
+++ sample/src/main/java/ru/slybeaver/truecalendar/MainActivity.kt
@@ -0,0 +1,72 @@
+package ru.slybeaver.truecalendar
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.slybeaver.slycalendarview.SlyCalendarDialog
+import ru.slybeaver.slycalendarview.SlyCalendarDialog.Callback
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Andreu on 16.11.2019.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val callback: Callback = object : Callback {
+            override fun onCancelled() {}
+            override fun onDataSelected(
+                    firstDate: Calendar?,
+                    secondDate: Calendar?,
+                    hours: Int,
+                    minutes: Int
+            ) {
+                if (firstDate != null) {
+                    if (secondDate == null) {
+                        firstDate[Calendar.HOUR_OF_DAY] = hours
+                        firstDate[Calendar.MINUTE] = minutes
+
+                        toast(SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(firstDate.time))
+                    } else {
+                        toast(getString(
+                                R.string.period,
+                                SimpleDateFormat(getString(R.string.dateFormat), Locale.getDefault()).format(firstDate.time),
+                                SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault()).format(secondDate.time)
+                        ))
+                    }
+                }
+            }
+        }
+
+        btnShowCalendar.setOnClickListener {
+            SlyCalendarDialog()
+                    .setSingle(false)
+                    //.setFirstMonday(false)
+                    .setCallback(callback)
+                    .show(supportFragmentManager, "TAG_SLYCALENDAR")
+        }
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        btnShowAndroid.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                toast("$dayOfMonth/$monthOfYear/$year")
+            }, year, month, day)
+            dpd.show()
+        }
+    }
+
+    fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+}
