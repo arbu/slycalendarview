712f517d1b3e7c3e496bfa39199ea4d791d266d8
andreu21-dev <andreu21@protonmail.com>
2019-11-16 13:39:57 +0300

    code clean

--- slycalendarview/src/main/java/ru/slybeaver/slycalendarview/SlyCalendarView.kt
+++ slycalendarview/src/main/java/ru/slybeaver/slycalendarview/SlyCalendarView.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
+import kotlinx.android.synthetic.main.slycalendar_frame.view.*
 import ru.slybeaver.slycalendarview.SlyCalendarDialog.Callback
 import ru.slybeaver.slycalendarview.listeners.DateSelectListener
 import ru.slybeaver.slycalendarview.listeners.DialogCompleteListener
@@ -45,148 +46,152 @@ class SlyCalendarView @JvmOverloads constructor(
     private fun init(attrs: AttributeSet?, defStyle: Int) {
         View.inflate(context, R.layout.slycalendar_frame, this)
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlyCalendarView, defStyle, 0)
-        if (slyCalendarData!!.backgroundColor == null) {
-            slyCalendarData!!.backgroundColor = typedArray.getColor(R.styleable.SlyCalendarView_backgroundColor, ContextCompat.getColor(context, R.color.slycalendar_defBackgroundColor))
+        if (slyCalendarData?.backgroundColor == null) {
+            slyCalendarData?.backgroundColor = typedArray.getColor(R.styleable.SlyCalendarView_backgroundColor, ContextCompat.getColor(context, R.color.slycalendar_defBackgroundColor))
         }
-        if (slyCalendarData!!.headerColor == null) {
-            slyCalendarData!!.headerColor = typedArray.getColor(R.styleable.SlyCalendarView_headerColor, ContextCompat.getColor(context, R.color.slycalendar_defHeaderColor))
+        if (slyCalendarData?.headerColor == null) {
+            slyCalendarData?.headerColor = typedArray.getColor(R.styleable.SlyCalendarView_headerColor, ContextCompat.getColor(context, R.color.slycalendar_defHeaderColor))
         }
-        if (slyCalendarData!!.headerTextColor == null) {
-            slyCalendarData!!.headerTextColor = typedArray.getColor(R.styleable.SlyCalendarView_headerTextColor, ContextCompat.getColor(context, R.color.slycalendar_defHeaderTextColor))
+        if (slyCalendarData?.headerTextColor == null) {
+            slyCalendarData?.headerTextColor = typedArray.getColor(R.styleable.SlyCalendarView_headerTextColor, ContextCompat.getColor(context, R.color.slycalendar_defHeaderTextColor))
         }
-        if (slyCalendarData!!.textColor == null) {
-            slyCalendarData!!.textColor = typedArray.getColor(R.styleable.SlyCalendarView_textColor, ContextCompat.getColor(context, R.color.slycalendar_defTextColor))
+        if (slyCalendarData?.textColor == null) {
+            slyCalendarData?.textColor = typedArray.getColor(R.styleable.SlyCalendarView_textColor, ContextCompat.getColor(context, R.color.slycalendar_defTextColor))
         }
-        if (slyCalendarData!!.selectedColor == null) {
-            slyCalendarData!!.selectedColor = typedArray.getColor(R.styleable.SlyCalendarView_selectedColor, ContextCompat.getColor(context, R.color.slycalendar_defSelectedColor))
+        if (slyCalendarData?.selectedColor == null) {
+            slyCalendarData?.selectedColor = typedArray.getColor(R.styleable.SlyCalendarView_selectedColor, ContextCompat.getColor(context, R.color.slycalendar_defSelectedColor))
         }
-        if (slyCalendarData!!.selectedTextColor == null) {
-            slyCalendarData!!.selectedTextColor = typedArray.getColor(R.styleable.SlyCalendarView_selectedTextColor, ContextCompat.getColor(context, R.color.slycalendar_defSelectedTextColor))
+        if (slyCalendarData?.selectedTextColor == null) {
+            slyCalendarData?.selectedTextColor = typedArray.getColor(R.styleable.SlyCalendarView_selectedTextColor, ContextCompat.getColor(context, R.color.slycalendar_defSelectedTextColor))
         }
         typedArray.recycle()
-        val vpager: ViewPager = findViewById(R.id.content)
-        vpager.adapter = MonthPagerAdapter(slyCalendarData!!, this)
-        vpager.currentItem = vpager.adapter!!.count / 2
+        val pager: ViewPager = findViewById(R.id.content)
+        pager.adapter = MonthPagerAdapter(slyCalendarData!!, this)
+        pager.currentItem = pager.adapter!!.count / 2
         showCalendar()
     }
 
     private fun showCalendar() {
         paintCalendar()
         showTime()
-        findViewById<View>(R.id.txtCancel).setOnClickListener {
+        txtCancel.setOnClickListener {
             if (callback != null) {
-                callback!!.onCancelled()
+                callback?.onCancelled()
             }
             if (completeListener != null) {
-                completeListener!!.complete()
+                completeListener?.complete()
             }
         }
-        findViewById<View>(R.id.txtSave).setOnClickListener {
+        txtSave.setOnClickListener {
             if (callback != null) {
                 var start: Calendar? = null
                 var end: Calendar? = null
-                if (slyCalendarData!!.selectedStartDate != null) {
+                if (slyCalendarData?.selectedStartDate != null) {
                     start = Calendar.getInstance()
-                    start.time = slyCalendarData!!.selectedStartDate
+                    start.time = slyCalendarData?.selectedStartDate
                 }
-                if (slyCalendarData!!.selectedEndDate != null) {
+                if (slyCalendarData?.selectedEndDate != null) {
                     end = Calendar.getInstance()
-                    end.time = slyCalendarData!!.selectedEndDate
+                    end.time = slyCalendarData?.selectedEndDate
                 }
-                callback!!.onDataSelected(start, end, slyCalendarData!!.selectedHour, slyCalendarData!!.selectedMinutes)
+                callback?.onDataSelected(start, end, slyCalendarData!!.selectedHour, slyCalendarData!!.selectedMinutes)
             }
             if (completeListener != null) {
-                completeListener!!.complete()
+                completeListener?.complete()
             }
         }
         val calendarStart = Calendar.getInstance()
         var calendarEnd: Calendar? = null
-        if (slyCalendarData!!.selectedStartDate != null) {
-            calendarStart.time = slyCalendarData!!.selectedStartDate
+        if (slyCalendarData?.selectedStartDate != null) {
+            calendarStart.time = slyCalendarData?.selectedStartDate
         } else {
-            calendarStart.time = slyCalendarData!!.showDate
+            calendarStart.time = slyCalendarData?.showDate
         }
-        if (slyCalendarData!!.selectedEndDate != null) {
+        if (slyCalendarData?.selectedEndDate != null) {
             calendarEnd = Calendar.getInstance()
-            calendarEnd.time = slyCalendarData!!.selectedEndDate
+            calendarEnd.time = slyCalendarData?.selectedEndDate
         }
-        (findViewById<View>(R.id.txtYear) as TextView).text = calendarStart[Calendar.YEAR].toString()
+        txtYear.text = calendarStart[Calendar.YEAR].toString()
         if (calendarEnd == null) {
-            (findViewById<View>(R.id.txtSelectedPeriod) as TextView).text = SimpleDateFormat("EE, dd MMMM", Locale.getDefault()).format(calendarStart.time)
+            txtSelectedPeriod.text = SimpleDateFormat("EE, dd MMMM", Locale.getDefault()).format(calendarStart.time)
         } else {
             if (calendarStart[Calendar.MONTH] == calendarEnd[Calendar.MONTH]) {
-                (findViewById<View>(R.id.txtSelectedPeriod) as TextView).text = context.getString(R.string.slycalendar_dates_period, SimpleDateFormat("EE, dd", Locale.getDefault()).format(calendarStart.time), SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarEnd.time))
+                txtSelectedPeriod.text = context.getString(R.string.slycalendar_dates_period, SimpleDateFormat("EE, dd", Locale.getDefault()).format(calendarStart.time), SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarEnd.time))
             } else {
-                (findViewById<View>(R.id.txtSelectedPeriod) as TextView).text = context.getString(R.string.slycalendar_dates_period, SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarStart.time), SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarEnd.time))
+                txtSelectedPeriod.text = context.getString(R.string.slycalendar_dates_period, SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarStart.time), SimpleDateFormat("EE, dd MMM", Locale.getDefault()).format(calendarEnd.time))
             }
         }
-        findViewById<View>(R.id.btnMonthPrev).setOnClickListener {
+        btnMonthPrev.setOnClickListener {
             val vpager: ViewPager = findViewById(R.id.content)
             vpager.currentItem = vpager.currentItem - 1
         }
-        findViewById<View>(R.id.btnMonthNext).setOnClickListener {
+        btnMonthNext.setOnClickListener {
             val vpager: ViewPager = findViewById(R.id.content)
             vpager.currentItem = vpager.currentItem + 1
         }
-        findViewById<View>(R.id.txtTime).setOnClickListener {
+        txtTime.setOnClickListener {
             var style = R.style.SlyCalendarTimeDialogTheme
-            if (slyCalendarData!!.timeTheme != null) {
-                style = slyCalendarData!!.timeTheme!!
+            if (slyCalendarData?.timeTheme != null) {
+                style = slyCalendarData?.timeTheme!!
             }
             val tpd = TimePickerDialog(context, style, OnTimeSetListener { view, hourOfDay, minute ->
-                slyCalendarData!!.selectedHour = hourOfDay
-                slyCalendarData!!.selectedMinutes = minute
+                slyCalendarData?.selectedHour = hourOfDay
+                slyCalendarData?.selectedMinutes = minute
                 showTime()
             }, slyCalendarData!!.selectedHour, slyCalendarData!!.selectedMinutes, true)
             tpd.show()
         }
-        val vpager: ViewPager = findViewById(R.id.content)
-        vpager.adapter!!.notifyDataSetChanged()
-        vpager.invalidate()
+        val pager: ViewPager = findViewById(R.id.content)
+        pager.adapter!!.notifyDataSetChanged()
+        pager.invalidate()
     }
 
     override fun dateSelect(selectedDate: Date) {
-        if (slyCalendarData!!.selectedStartDate == null || slyCalendarData!!.single) {
-            slyCalendarData!!.selectedStartDate = selectedDate
+        if (slyCalendarData?.selectedStartDate == null || slyCalendarData!!.single) {
+            slyCalendarData?.selectedStartDate = selectedDate
             showCalendar()
             return
         }
-        if (slyCalendarData!!.selectedEndDate == null) {
-            if (selectedDate.time < slyCalendarData!!.selectedStartDate!!.time) {
-                slyCalendarData!!.selectedEndDate = slyCalendarData!!.selectedStartDate
-                slyCalendarData!!.selectedStartDate = selectedDate
-                showCalendar()
-                return
-            } else if (selectedDate.time == slyCalendarData!!.selectedStartDate!!.time) {
-                slyCalendarData!!.selectedEndDate = null
-                slyCalendarData!!.selectedStartDate = selectedDate
-                showCalendar()
-                return
-            } else if (selectedDate.time > slyCalendarData!!.selectedStartDate!!.time) {
-                slyCalendarData!!.selectedEndDate = selectedDate
-                showCalendar()
-                return
+        if (slyCalendarData?.selectedEndDate == null) {
+            when {
+                selectedDate.time < slyCalendarData?.selectedStartDate!!.time -> {
+                    slyCalendarData?.selectedEndDate = slyCalendarData?.selectedStartDate
+                    slyCalendarData?.selectedStartDate = selectedDate
+                    showCalendar()
+                    return
+                }
+                selectedDate.time == slyCalendarData?.selectedStartDate!!.time -> {
+                    slyCalendarData?.selectedEndDate = null
+                    slyCalendarData?.selectedStartDate = selectedDate
+                    showCalendar()
+                    return
+                }
+                selectedDate.time > slyCalendarData?.selectedStartDate!!.time -> {
+                    slyCalendarData?.selectedEndDate = selectedDate
+                    showCalendar()
+                    return
+                }
             }
         }
-        if (slyCalendarData!!.selectedEndDate != null) {
-            slyCalendarData!!.selectedEndDate = null
-            slyCalendarData!!.selectedStartDate = selectedDate
+        if (slyCalendarData?.selectedEndDate != null) {
+            slyCalendarData?.selectedEndDate = null
+            slyCalendarData?.selectedStartDate = selectedDate
             showCalendar()
         }
     }
 
     override fun dateLongSelect(selectedDate: Date) {
-        slyCalendarData!!.selectedEndDate = null
-        slyCalendarData!!.selectedStartDate = selectedDate
+        slyCalendarData?.selectedEndDate = null
+        slyCalendarData?.selectedStartDate = selectedDate
         showCalendar()
     }
 
     private fun paintCalendar() {
-        findViewById<View>(R.id.mainFrame).setBackgroundColor(slyCalendarData!!.backgroundColor!!)
-        findViewById<View>(R.id.headerView).setBackgroundColor(slyCalendarData!!.headerColor!!)
-        (findViewById<View>(R.id.txtYear) as TextView).setTextColor(slyCalendarData!!.headerTextColor!!)
-        (findViewById<View>(R.id.txtSelectedPeriod) as TextView).setTextColor(slyCalendarData!!.headerTextColor!!)
-        (findViewById<View>(R.id.txtTime) as TextView).setTextColor(slyCalendarData!!.headerColor!!)
+        mainFrame.setBackgroundColor(slyCalendarData?.backgroundColor!!)
+        headerView.setBackgroundColor(slyCalendarData?.headerColor!!)
+        txtYear.setTextColor(slyCalendarData?.headerTextColor!!)
+        txtSelectedPeriod.setTextColor(slyCalendarData?.headerTextColor!!)
+        txtTime.setTextColor(slyCalendarData?.headerColor!!)
     }
 
     private fun showTime() {
